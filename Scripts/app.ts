fb10d355981fb6184838df537b3003c438d16509
Nik <nikeshpatel356@gmail.com>
2019-01-19 10:38:07 -0500

    Error solve in sudent.ts

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -14,68 +14,6 @@
 //     window.addEventListener("load", Start);
 
 // })();
-module objects {
-   
-export abstract class Person  {
-    // PRIVATE INSTANCE VARIABLES
-    private _age: number;
-    private _name: string;
-
-    // PUBLIC PROPERTIES
-    get age(): number {
-        return this._age;
-    }
-
-    set age(newAge: number) {
-        this._age = newAge;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    set name(newName: string) {
-        this._name = newName;
-    }
-
-    constructor(age: number, name:string) {
-        this.age = age;
-        this.name = name;
-    }
-
-    public saysHello():void {
-        console.log(`${this.name} says Hello!`);
-    }
-    }
-
-    export class Student extends objects.Person {
-        // PRIVATE INSTANCE VARIABLES
-        private _studentID: string;
-
-        // PUBLIC PROPERTIES
-        get studentID(): string {
-            return this._studentID;
-        }
-
-        set studentID(newStudentID: string) {
-            this._studentID = newStudentID;
-        }
-
-        constructor(age:number, name:string, studentID:string) {
-            super(age, name);
-
-            this.studentID = studentID;
-        }
-
-        // PRIVATE METHODS
-
-        // PUBLIC METHODS
-        public studies(): void {
-            console.log(`${this.name} with studentID: ${this.studentID} is studying.`)
-        }
-    }
-
-}
 
 
 /*
